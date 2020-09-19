98c8bd982b1b68371d2d28457df275573cc67056
Jessica P <blueraindrop@users.noreply.github.com>
2020-09-18 23:02:37 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+// Iteration 1: Names and Input
+
+let hacker1 = "Harry";
+console.log(`The driver's name is ${hacker1}.`);
+
+let hacker2 = "Chrome";
+console.log(`The navigator's name is ${hacker2}`);
+
+// Iteration 2: Conditionals
+// Here we will review which name has the most characters
+
+if (hacker1.length > hacker2.length) {
+console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+}
+else if 
+   (hacker2.length > hacker1.length) {
+console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)}
+
+else if
+   (hacker2.length = hacker2.length) {
+console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+ }
+
+ // Iteration 3: Loops
+
+hacker1.toUpperCase();
+ 
+for (let value of hacker1) {
+   console.log(value.toUpperCase());
+ }
+ 
